7ff20efaf7a2e52979d381075fcdecf80eb98f62
mabhedla-nkosi <nkosi.mlambo@gmail.com>
2022-02-08 11:24:53 +0200

    swagger configurations
    I added swagger configurations in the pom.xml, properties, and a class

---
+++ src/main/java/com/buildersrating/buildersrating/swaggerconfiguration/SwaggerConfigurations.java
@@ -0,0 +1,23 @@
+package com.buildersrating.buildersrating.swaggerconfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfigurations {
+
+    @Bean
+    public Docket api2() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.buildersrating.buildersrating"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
