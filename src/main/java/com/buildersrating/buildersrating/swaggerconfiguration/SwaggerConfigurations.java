1dfd70a4b88763ad9b2631d3dccbf07a05a479ce
mabhedla-nkosi <nkosi.mlambo@gmail.com>
2022-02-12 22:30:12 +0200

    updated swagger configurations

--- src/main/java/com/buildersrating/buildersrating/swaggerconfiguration/SwaggerConfigurations.java
+++ src/main/java/com/buildersrating/buildersrating/swaggerconfiguration/SwaggerConfigurations.java
@@ -2,6 +2,9 @@ package com.buildersrating.buildersrating.swaggerconfiguration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -10,14 +13,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@EnableWebMvc
 public class SwaggerConfigurations {
 
     @Bean
-    public Docket api2() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.buildersrating.buildersrating"))
                 .paths(PathSelectors.any())
                 .build();
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer()
+    {
+        return new WebMvcConfigurer()
+        {
+            @Override
+            public void addResourceHandlers( ResourceHandlerRegistry registry )
+            {
+                registry.addResourceHandler( "swagger-ui.html" ).addResourceLocations( "classpath:/META-INF/resources/" );
+                registry.addResourceHandler( "/webjars/**" ).addResourceLocations( "classpath:/META-INF/resources/webjars/" );
+            }
+        };
     }
 }
